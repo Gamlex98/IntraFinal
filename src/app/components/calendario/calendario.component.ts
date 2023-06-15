50704976d901e3976619dc208a939e0bfdc8c5e6
Fredy Alexander FAOC. Obando Cordoba <aprendiz.desarrollo@fresmar.local>
2023-06-15 17:00:25 -0500

    Actualizacion Calendar

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -10,6 +10,8 @@ import { CalendarService } from 'src/app/services/calendar.service';
 import { EventModel } from 'src/app/models/event.model'; 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './modal/modal.component';
+import Swal from 'sweetalert2';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -19,8 +21,8 @@ import { ModalComponent } from './modal/modal.component';
 })
 export class CalendarioComponent {
 
+  fechaSeleccionada !: string;
   calendarVisible = true;
-  startDate !: Date;
   calendarOptions: CalendarOptions = {
     plugins: [
       interactionPlugin,
@@ -61,9 +63,14 @@ export class CalendarioComponent {
   }
 
   handleDateSelect(selectInfo: DateSelectArg) {
+  this.fechaSeleccionada = selectInfo.startStr;
+  console.log(this.fechaSeleccionada);
+
   const modalRef = this.modalService.open(ModalComponent);
+  modalRef.componentInstance.fechaSeleccionada = this.fechaSeleccionada; // Pasar la fecha seleccionada al modal
   modalRef.result.then((result) => {
     if (result) {
+      console.log('Result Modal:',result);
       const { titulo, fechaStart, fechaEnd } = result;
       const calendarApi = selectInfo.view.calendar;
   
@@ -86,8 +93,14 @@ export class CalendarioComponent {
       eventoGuardar.start = fechaStart;
       eventoGuardar.end = fechaEnd;
       this.grabarEventoBD(eventoGuardar);
-
       this.refreshEvents(); 
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: `Evento : <span style="color: blue; text-decoration: underline">${titulo}</span>  guardado Existosamente !`,
+        showConfirmButton: false,
+        timer: 1000
+        });
       }
     }, (reason) => {
       console.log(reason);
@@ -105,17 +118,43 @@ export class CalendarioComponent {
   }
 
   handleEventClick(clickInfo: EventClickArg) {
-    if (confirm(`¿ ESTAS SEGURO QUE DESEAS ELIMINAR ESTE EVENTO ? '${clickInfo.event.title}'`)) {
-      console.log(clickInfo.event.id);
-      // clickInfo.event.remove();
-      this.calendarService.removeEvent(clickInfo.event.id).subscribe({
-        next: (data:any)=>{
-            console.log("evento borrado de la BD",data);
-            this.refreshEvents(); 
-          },
-        error:(e)=> console.log(e)
-        });
-    }
+    Swal.fire({
+      title: `Estas seguro que deseas eliminar este evento? <span style="color: red; text-decoration: underline">${clickInfo.event.title}</span>`,
+      text: "No podras deshacer esta accion.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.calendarService.removeEvent(clickInfo.event.id).subscribe({
+          next: (data:any)=>{
+              console.log("evento borrado de la BD",data);
+              this.refreshEvents(); 
+              Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Evento Borrado Exitosamente !!',
+                showConfirmButton: false,
+                timer: 1000
+                });
+            },
+          error:(e)=> console.log(e)
+          });
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: `Cancelado`,
+          text:'Tu evento esta seguro :)',
+          showConfirmButton: false,
+          timer: 1000
+          });
+      }
+    })
   }
 
   handleEvents(events: EventApi[]) {
@@ -128,8 +167,37 @@ export class CalendarioComponent {
     });
     this.changeDetector.detectChanges();
   }
-  
 
+  //OBTENER EVENTOS DIA ACTUAL 
+  
+  /* handleEvents(events: EventApi[]) {
+    // Obtener la fecha actual
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+  
+    // Filtrar los eventos por fecha
+    this.currentEvents = events.filter(event => {
+      const eventStart = event.start?.getTime() || 0;
+      const eventYear = event.start?.getFullYear() || 0;
+      const eventMonth = event.start?.getMonth() || 0;
+      const eventDay = event.start?.getDate() || 0;
+  
+      return eventYear === currentYear && eventMonth === currentMonth && eventDay === currentDay;
+    });
+  
+    // Ordenar los eventos por fecha de inicio en orden ascendente
+    this.currentEvents.sort((a, b) => {
+      const aStart = a.start?.getTime() || 0;
+      const bStart = b.start?.getTime() || 0;
+  
+      return aStart - bStart;
+    });
+  
+    this.changeDetector.detectChanges();
+  } */
+  
   //Graba en la base de datos el evento ingresado por el usuario
   grabarEventoBD(eventoGrabar: EventModel){
     this.calendarService.addEvent(eventoGrabar).subscribe({
--- src/app/components/calendario/modal/modal.component.ts
+++ src/app/components/calendario/modal/modal.component.ts
@@ -1,26 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { CalendarService } from 'src/app/services/calendar.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
-  titulo !: string;
-  fechaInicio !: string;
+export class ModalComponent implements OnInit{
+  @Input() fechaSeleccionada!: string;
+
   fechaFin !: string;
+  fechaInicial !: string;
+  fechaInicialDate !: Date;
+  tituloEvento !: string;
 
   constructor(public modal: NgbActiveModal) {}
 
-  guardarEvento() {
-    const evento = {
-      titulo: this.titulo,
-      fechaStart: new Date(this.fechaInicio),
-      fechaEnd: new Date(this.fechaFin)
-    };
-    this.modal.close(evento);
+  ngOnInit(): void {
+    console.log('FechaModal:', this.fechaSeleccionada);
+  
+    // Convertir fechaSeleccionada a un objeto Date
+    this.fechaInicialDate = new Date(this.fechaSeleccionada);
+    this.fechaInicialDate.setUTCHours(12, 0, 0, 0);
+  
+    // Formatear la fecha inicial en el formato adecuado para el campo de fecha
+    const year = this.fechaInicialDate.getUTCFullYear();
+    const month = (this.fechaInicialDate.getUTCMonth() + 1).toString().padStart(2, '0');
+    const day = this.fechaInicialDate.getUTCDate().toString().padStart(2, '0');
+    const hours = this.fechaInicialDate.getUTCHours().toString().padStart(2, '0');
+    const minutes = this.fechaInicialDate.getUTCMinutes().toString().padStart(2, '0');
+
+    this.fechaInicial = `${year}-${month}-${day}T${hours}:${minutes}`;
+    this.fechaFin = `${year}-${month}-${day}T${hours}:${minutes}`;
   }
+  
+  guardarEvento() {
+  const fechaActual = new Date();
+  fechaActual.setHours(0, 0, 0, 0); // Establecer la hora a 00:00:00
+  
+  const fechaInicial = new Date(this.fechaInicialDate);
+  fechaInicial.setHours(0, 0, 0, 0); // Establecer la hora a 00:00:00
+  
+  if (fechaInicial < fechaActual) {
+    // Mostrar mensaje de error o realizar la acción correspondiente
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: `Fecha incorrecta !`,
+      text: 'Debes seleccionar una fecha igual o posterior a la actual!',
+      showConfirmButton: false,
+      timer: 1000
+    });
+    console.log("Error: La fecha de inicio debe ser igual o posterior al día actual.");
+    return;
+  }
+
+  const evento = {
+    titulo: this.tituloEvento,
+    fechaStart: new Date(this.fechaInicial),
+    fechaEnd: new Date(this.fechaFin)
+  };
+  this.modal.close(evento);
+}
+
 }
 
