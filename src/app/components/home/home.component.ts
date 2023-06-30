02200bc35d08c26664cc82d68b0446cf11c7b92e
Fredy Alexander FAOC. Obando Cordoba <aprendiz.desarrollo@fresmar.local>
2023-06-30 16:58:02 -0500

    Update 5

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+declare const StickyNavigation: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit , OnDestroy {
 
+  ngOnInit() {
+    new StickyNavigation();
+  }
+
+  ngOnDestroy() {
+    // Limpiar eventos y objetos relacionados con StickyNavigation para eliminar errores al cambiar de Componente en la NavBar Principal
+    $(window).off('scroll');
+    $(window).off('resize');
+    $('.et-hero-tab').off('click');
+    
+    // Si es necesario, llamar a métodos adicionales de limpieza en StickyNavigation
+    // this.stickyNavigation.cleanupMethod();
+  }
 }
